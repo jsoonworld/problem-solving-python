7f8dcb69c3c13eb50faa54db7847416007a66fd6
권장순 <jsoonworld@gmail.com>
2024-12-28 22:06:54 +0900

    feat(find_not_repeating_first_character) : solved problem

---
+++ 1st_week/01_05_find_not_repeating_first_character/first.py
@@ -0,0 +1,19 @@
+input = "abadabac"
+
+def find_not_repeating_first_character(string):
+    arr = [0] * 26
+
+    for char in string:
+        arr[ord(char) - ord('a')] += 1
+
+    for char in string:
+        if arr[ord(char) - ord('a')] == 1:
+            return char
+
+    return "_"
+
+
+result = find_not_repeating_first_character
+print("정답 = d 현재 풀이 값 =", result("abadabac"))
+print("정답 = c 현재 풀이 값 =", result("aabbcddd"))
+print("정답 =_ 현재 풀이 값 =", result("aaaaaaaa"))
