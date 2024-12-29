d0c5ecd2cd70a468fcd45da5da3ca2263d9fe6cc
권장순 <jsoonworld@gmail.com>
2024-12-29 09:19:41 +0900

    refactor(find_max_num) : reviewed and improved code

--- inflearn_dinco_problem/1st_week/01_01_find_max_num/second.py
+++ inflearn_dinco_problem/1st_week/01_01_find_max_num/second.py
@@ -1,3 +1,12 @@
+# def find_max_num(array):
+#     for number in array:
+#         is_max_num = True
+#         for compare_number in array:
+#             if number < compare_number:
+#                 is_max_num = False
+#         if is_max_num:
+#             return number
+
 def find_max_num(array):
     for number in array:
         is_max_num = True
@@ -6,6 +15,7 @@ def find_max_num(array):
                 is_max_num = False
         if is_max_num:
             return number
+
 
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
--- inflearn_dinco_problem/1st_week/01_01_find_max_num/third.py
+++ inflearn_dinco_problem/1st_week/01_01_find_max_num/third.py
@@ -1,3 +1,11 @@
+# def find_max_num(array):
+#     max_number = array[0]
+#
+#     for number in array:
+#         if number > max_number:
+#             max_number = number
+#     return max_number
+
 def find_max_num(array):
     max_number = array[0]
 
