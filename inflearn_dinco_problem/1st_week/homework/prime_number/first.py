b792626d1a62bb60fc28a07202cbf55dc98f82eb
권장순 <jsoonworld@gmail.com>
2024-12-29 09:55:07 +0900

    refactor(find_prime_list_under_number) : reviewed and improved code

--- inflearn_dinco_problem/1st_week/homework/prime_number/first.py
+++ inflearn_dinco_problem/1st_week/homework/prime_number/first.py
@@ -1,5 +1,15 @@
 input = 20
 
+# def find_prime_list_under_number(number):
+#     prime_list = []
+#     for n in range(2, number +1):
+#         for i in range(2, n):
+#             if n % i == 0:
+#                 break
+#             else:
+#                 prime_list.append(n)
+#     return prime_list
+
 def find_prime_list_under_number(number):
     prime_list = []
     for n in range(2, number +1):
