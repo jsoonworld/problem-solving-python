6251538b0ef0d8527635e8bdce51524d45e09210
권장순 <jsoonworld@gmail.com>
2024-12-30 12:43:06 +0900

    refactor(summarize_string) : reviewed and improved code

--- inflearn_dinco_problem/1st_week/homework/summarize_string/third.py
+++ inflearn_dinco_problem/1st_week/homework/summarize_string/third.py
@@ -1,3 +1,18 @@
+# def summarize_string(input_str):
+#     result = []
+#     count = 1
+#     append_alpha = input_str[0]
+#
+#     for i in range(1, len(input_str)):
+#         if input_str[i] != append_alpha:
+#             result.append(append_alpha + str(count))
+#             append_alpha = input_str[i]
+#             count = 1
+#         else:
+#             count += 1
+#     result.append(append_alpha + str(count))
+#     return "/".join(result)
+
 def summarize_string(input_str):
     result = []
     count = 1
@@ -12,6 +27,7 @@ def summarize_string(input_str):
             count += 1
     result.append(append_alpha + str(count))
     return "/".join(result)
+
 
 input_str = "acccdeee"
 print(summarize_string(input_str))
