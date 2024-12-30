8dadc95ac2c48b6e70ae0ae7ba9bf195ac15915c
권장순 <jsoonworld@gmail.com>
2024-12-30 12:42:28 +0900

    refactor(find_count_to_turn_out_to_all_zero_or_all_one) : reviewed and improved code

--- inflearn_dinco_problem/1st_week/homework/find_count_to_turn_out_to_all_zero_or_all_one/third.py
+++ inflearn_dinco_problem/1st_week/homework/find_count_to_turn_out_to_all_zero_or_all_one/third.py
@@ -1,20 +1,40 @@
+# def find_count_to_turn_out_to_all_zero_or_all_one(string):
+#     count_to_all_zero = 0
+#     count_to_all_one = 0
+#
+#     if string[0] == '0':
+#         count_to_all_one +=1
+#     else:
+#         count_to_all_zero += 1
+#
+#     for i in range(len(string)-1):
+#         if string[i] != string[i+1]:
+#             if string[i+1] == '1':
+#                 count_to_all_zero += 1
+#             if string[i+1] == '0':
+#                 count_to_all_one += 1
+#
+#     return min(count_to_all_zero, count_to_all_one)
+
 def find_count_to_turn_out_to_all_zero_or_all_one(string):
     count_to_all_zero = 0
     count_to_all_one = 0
 
     if string[0] == '0':
-        count_to_all_one +=1
+        count_to_all_one += 1
     else:
         count_to_all_zero += 1
 
-    for i in range(len(string)-1):
+    for i in range(len(string) - 1):
         if string[i] != string[i+1]:
-            if string[i+1] == '1':
+            if string[i + 1] == '1':
                 count_to_all_zero += 1
             if string[i+1] == '0':
                 count_to_all_one += 1
-
     return min(count_to_all_zero, count_to_all_one)
+
+
+
 
 
 input = "011110"
