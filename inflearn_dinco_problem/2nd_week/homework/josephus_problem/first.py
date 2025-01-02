90e769e681dd8ad569b69257b921ff90ce370cf4
권장순 <jsoonworld@gmail.com>
2025-01-02 10:09:03 +0900

    refactoring(josephus_problem) : reviewed solved problem

--- inflearn_dinco_problem/2nd_week/homework/josephus_problem/first.py
+++ inflearn_dinco_problem/2nd_week/homework/josephus_problem/first.py
@@ -1,6 +1,16 @@
 
 input = "7 3"
-def josephus_problem(n, k):
+# def josephus_problem(n, k):
+#     array = list(range(1, n+1))
+#     result = []
+#     target_index = 0
+#
+#     while array:
+#         target_index = (target_index + k - 1) % len(array)
+#         result.append(array.pop(target_index))
+#     return f"<{', '.join(map(str, result))}>"
+
+def josephus_problem(n,k):
     array = list(range(1, n+1))
     result = []
     target_index = 0
@@ -8,7 +18,8 @@ def josephus_problem(n, k):
     while array:
         target_index = (target_index + k - 1) % len(array)
         result.append(array.pop(target_index))
-    return f"<{', '.join(map(str, result))}>"
+    return f"<{','.join(map(str, result))}>"
+
 
 n, k = map(int, input.split())
 print(josephus_problem(n, k))
