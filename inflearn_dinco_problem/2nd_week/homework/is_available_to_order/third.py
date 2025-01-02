1c2bea4e75fc90bd6d4760dce753fa650243c707
권장순 <jsoonworld@gmail.com>
2025-01-02 10:05:38 +0900

    refactoring(is_available_to_order) : reviewed solved problem

--- inflearn_dinco_problem/2nd_week/homework/is_available_to_order/third.py
+++ inflearn_dinco_problem/2nd_week/homework/is_available_to_order/third.py
@@ -1,6 +1,28 @@
 shop_menus = ["만두", "떡볶이", "오뎅", "사이다", "콜라"]
 shop_orders = ["오뎅", "콜라", "만두"]
 
+# def is_available_to_order(menus, orders):
+#     menus.sort()
+#
+#     def binary_search(target, data):
+#         start = 0
+#         end = len(data) - 1
+#
+#         while start <= end:
+#             mid = (start + end) // 2
+#             if data[mid] == target:
+#                 return True
+#             elif data[mid] < target:
+#                 start = mid + 1
+#             else:
+#                 end = mid - 1
+#         return False
+#
+#     for order in orders:
+#         if not binary_search(order, menus):
+#             return False
+#         return True
+
 def is_available_to_order(menus, orders):
     menus.sort()
 
