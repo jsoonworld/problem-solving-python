251556388f7ff30198c8ab2473703965341d9661
권장순 <jsoonworld@gmail.com>
2025-01-03 10:01:19 +0900

    refactoring(bubble_sort) : reviewed solved problem

--- inflearn_dinco_problem/3rd_week/03_01_buble_sort/firsst.py
+++ inflearn_dinco_problem/3rd_week/03_01_buble_sort/firsst.py
@@ -1,15 +1,21 @@
 input = [4, 6, 2, 9, 1]
 
 
+# def bubble_sort(array):
+#     for i in range(len(array)):
+#         for j in range(len(array) -1):
+#             if array[j] > array[j+1]:
+#                 temp = array[j]
+#                 array[j] = array[j+1]
+#                 array[j+1] = temp
+#     return array
+
 def bubble_sort(array):
     for i in range(len(array)):
-        for j in range(len(array) -1):
+        for j in range(len(array) - 1):
             if array[j] > array[j+1]:
-                temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
+                array[j], array[j+1] = array[j+1], array[j]
     return array
-
 
 bubble_sort(input)
 print(input)  # [1, 2, 4, 6, 9] 가 되어야 합니다!
--- inflearn_dinco_problem/3rd_week/03_01_buble_sort/third.py
+++ inflearn_dinco_problem/3rd_week/03_01_buble_sort/third.py
@@ -1,6 +1,16 @@
 input = [4, 6, 2, 9, 1]
 
 
+# def bubble_sort(array):
+#     n = len(array)
+#
+#     for i in range(n - 1):
+#         for j in range(n - i - 1):
+#             if array[j] > array[j+1]:
+#                 array[j], array[j+1] = array[j+1], array[j]
+#     return array
+
+
 def bubble_sort(array):
     n = len(array)
 
@@ -9,6 +19,8 @@ def bubble_sort(array):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
     return array
+
+
 
 
 bubble_sort(input)
