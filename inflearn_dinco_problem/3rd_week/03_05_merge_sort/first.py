405ebc370e72413de8150d7237f7a0eca22ac986
권장순 <jsoonworld@gmail.com>
2025-01-03 10:59:08 +0900

    refactoring(merge_sort) : reviewed solved problem

--- inflearn_dinco_problem/3rd_week/03_05_merge_sort/first.py
+++ inflearn_dinco_problem/3rd_week/03_05_merge_sort/first.py
@@ -1,14 +1,27 @@
 array = [5, 3, 2, 1, 6, 8, 7, 4]
 
 
+# def merge_sort(array):
+#     if len(array) <= 1:
+#         return array
+#     mid = (0 + len(array)) // 2
+#     left_array = merge_sort(array[:mid])
+#     right_array = merge_sort(array[mid:])
+#     merge(left_array, right_array)
+#     return array
+
 def merge_sort(array):
     if len(array) <= 1:
         return array
+
     mid = (0 + len(array)) // 2
+
     left_array = merge_sort(array[:mid])
     right_array = merge_sort(array[mid:])
     merge(left_array, right_array)
+
     return array
+
 
 
 def merge(array1, array2):
